d6f5ca1abe8114174bee4c0949b846f779035451
Sachin Purohit <sachinpurohit@google.com>
2023-04-11 11:01:43 +0000

    impl(tests): Added initial set of tests

---
+++ odbc/connection.h
@@ -0,0 +1,44 @@
+
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <iodbcext.h>
+#include <locale.h>
+#include <sql.h>
+#include <sqlext.h>
+#include <sqlucode.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+namespace odbc {
+
+extern const char * kDefaultConnectionString;
+
+struct ConnectionHandle {
+  HENV henv;
+  HDBC hdbc;
+  HSTMT hstmt;
+  bool connected;
+  SQLCHAR outdsn[4096];
+};
+
+SQLRETURN Connect(char *conn_str, ConnectionHandle *conn);
+SQLRETURN Disconnect(ConnectionHandle *conn);
+SQLRETURN GetDriverInfo(ConnectionHandle *conn);
+SQLRETURN GetEnvInfo(ConnectionHandle *conn);
+SQLRETURN GetDescRec(ConnectionHandle *conn);
+SQLRETURN PrintDriverVerName(ConnectionHandle *conn);
+
+}
---
+++ tests/connection_test.cc
@@ -0,0 +1,46 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "odbc/connection.h"
+#include <gtest/gtest.h>
+
+using namespace odbc;
+
+TEST(ConnectionTest, SQLDriverConnect) {
+  ConnectionHandle conn;
+  EXPECT_EQ(Connect((char *)kDefaultConnectionString, &conn), SQL_SUCCESS);
+  EXPECT_EQ(Disconnect(&conn), SQL_SUCCESS);
+}
+
+TEST(DriverInfoTest, SQLGetInfo) {
+  ConnectionHandle conn;
+  EXPECT_EQ(Connect((char *)kDefaultConnectionString, &conn), SQL_SUCCESS);
+  EXPECT_EQ(GetDriverInfo(&conn), SQL_SUCCESS);
+  EXPECT_EQ(Disconnect(&conn), SQL_SUCCESS);
+}
+
+
+TEST(DriverAttributesTest, SQLGetEnvAttr) {
+  ConnectionHandle conn;
+  EXPECT_EQ(Connect((char *)kDefaultConnectionString, &conn), SQL_SUCCESS);
+  EXPECT_EQ(GetEnvInfo(&conn), SQL_SUCCESS);
+  EXPECT_EQ(Disconnect(&conn), SQL_SUCCESS);
+}
+
+TEST(DescriptorFieldsTest, SQLGetDescRec) {
+  ConnectionHandle conn;
+  EXPECT_EQ(Connect((char *)kDefaultConnectionString, &conn), SQL_SUCCESS);
+  EXPECT_EQ(GetDescRec(&conn), SQL_SUCCESS);
+  EXPECT_EQ(Disconnect(&conn), SQL_SUCCESS);
+}
