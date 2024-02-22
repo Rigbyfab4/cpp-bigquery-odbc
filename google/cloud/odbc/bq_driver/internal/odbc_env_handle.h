ed6028bcec1bda50e9f61fe4a91afe3b2bf0ba64
jsrinnn <114950032+jsrinnn@users.noreply.github.com>
2024-02-22 07:47:41 -0800

    impl(bq_driver): Core internal implementation of environment attribute methods (#226)

--- google/cloud/odbc/bq_driver/internal/odbc_env_handle.h
+++ google/cloud/odbc/bq_driver/internal/odbc_env_handle.h
@@ -17,12 +17,103 @@
 
 #include "google/cloud/odbc/bq_driver/internal/odbc_handle.h"
 #include "google/cloud/odbc/internal/odbc_includes.h"
+#include "google/cloud/status_or.h"
+#include <memory>
 
 namespace google::cloud::odbc_bq_driver_internal {
 
+enum EnvAttrConnectionPoolVal { kCpOff, kOnePerDriver, kOnePerHenv };
+enum EnvAttrCPMatchVal { kStrictMatch, kRelaxedMatch };
+enum EnvAttrOdbcVersVal { kOdbc2, kOdbc3 };
+enum EnvAttrOutputNTSVal { kNtsTrue };
+
+class EnvAttrConnectionPool {
+ public:
+  explicit EnvAttrConnectionPool() : name_("SQL_CP_OFF"), val_(SQL_CP_OFF) {}
+  explicit EnvAttrConnectionPool(EnvAttrConnectionPoolVal const& val);
+  ~EnvAttrConnectionPool() = default;
+
+  EnvAttrConnectionPool(EnvAttrConnectionPool const&) = default;
+  EnvAttrConnectionPool& operator=(EnvAttrConnectionPool const&) = default;
+  EnvAttrConnectionPool(EnvAttrConnectionPool&&) = default;
+  EnvAttrConnectionPool& operator=(EnvAttrConnectionPool&&) = default;
+  // TODO(b/326401873): Replace with StatusRecordOr.
+  static StatusOr<EnvAttrConnectionPoolVal> ParseVal(void* value);
+  [[nodiscard]] std::string Name() const { return name_; }
+  [[nodiscard]] SQLUINTEGER Value() const { return val_; }
+
+ private:
+  std::string name_;
+  SQLUINTEGER val_;
+};
+
+class EnvAttrConnectionPoolMatch {
+ public:
+  explicit EnvAttrConnectionPoolMatch()
+      : name_("SQL_CP_STRICT_MATCH"), val_(SQL_CP_STRICT_MATCH) {}
+  explicit EnvAttrConnectionPoolMatch(EnvAttrCPMatchVal const& val);
+  ~EnvAttrConnectionPoolMatch() = default;
+
+  EnvAttrConnectionPoolMatch(EnvAttrConnectionPoolMatch const&) = default;
+  EnvAttrConnectionPoolMatch& operator=(EnvAttrConnectionPoolMatch const&) =
+      default;
+  EnvAttrConnectionPoolMatch(EnvAttrConnectionPoolMatch&&) = default;
+  EnvAttrConnectionPoolMatch& operator=(EnvAttrConnectionPoolMatch&&) = default;
+
+  // TODO(b/326401873): Replace with StatusRecordOr.
+  static StatusOr<EnvAttrCPMatchVal> ParseVal(void* value);
+  [[nodiscard]] std::string Name() const { return name_; }
+  [[nodiscard]] SQLUINTEGER Value() const { return val_; }
+
+ private:
+  std::string name_;
+  SQLUINTEGER val_;
+};
+
+class EnvAttrOdbcVersion {
+ public:
+  explicit EnvAttrOdbcVersion() : name_("SQL_OV_ODBC3"), val_(SQL_OV_ODBC3) {}
+  explicit EnvAttrOdbcVersion(EnvAttrOdbcVersVal const& val);
+  ~EnvAttrOdbcVersion() = default;
+
+  EnvAttrOdbcVersion(EnvAttrOdbcVersion const&) = default;
+  EnvAttrOdbcVersion& operator=(EnvAttrOdbcVersion const&) = default;
+  EnvAttrOdbcVersion(EnvAttrOdbcVersion&&) = default;
+  EnvAttrOdbcVersion& operator=(EnvAttrOdbcVersion&&) = default;
+
+  // TODO(b/326401873): Replace with StatusRecordOr.
+  static StatusOr<EnvAttrOdbcVersVal> ParseVal(void* value);
+  [[nodiscard]] std::string Name() const { return name_; }
+  [[nodiscard]] SQLUINTEGER Value() const { return val_; }
+
+ private:
+  std::string name_;
+  SQLINTEGER val_;
+};
+
+class EnvAttrOutputNTS {
+ public:
+  explicit EnvAttrOutputNTS() = default;
+  ~EnvAttrOutputNTS() = default;
+
+  EnvAttrOutputNTS(EnvAttrOutputNTS const&) = default;
+  EnvAttrOutputNTS& operator=(EnvAttrOutputNTS const&) = default;
+  EnvAttrOutputNTS(EnvAttrOutputNTS&&) = default;
+  EnvAttrOutputNTS& operator=(EnvAttrOutputNTS&&) = default;
+
+  // TODO(b/326401873): Replace with StatusRecordOr.
+  static Status ParseVal(void* value);
+  [[nodiscard]] std::string Name() const { return name_; }
+  [[nodiscard]] SQLUINTEGER Value() const { return val_; }
+
+ private:
+  std::string name_{"SQL_TRUE"};
+  SQLINTEGER val_{SQL_TRUE};
+};
+
 class EnvironmentHandle : public Handle {
  public:
-  explicit EnvironmentHandle() = default;
+  explicit EnvironmentHandle();
   ~EnvironmentHandle() = default;
 
   EnvironmentHandle(EnvironmentHandle const&) = default;
@@ -33,6 +124,12 @@ class EnvironmentHandle : public Handle {
   SQLRETURN GetAttribute(SQLINTEGER attribute, void* value, void* length);
 
   SQLRETURN SetAttribute(SQLINTEGER attribute, void* value, void* length);
+
+ private:
+  std::shared_ptr<EnvAttrConnectionPool> connection_pool_;
+  std::shared_ptr<EnvAttrConnectionPoolMatch> connection_pool_match_;
+  std::shared_ptr<EnvAttrOdbcVersion> odbc_ver_;
+  std::shared_ptr<EnvAttrOutputNTS> output_nts_;
 };
 
 }  // namespace google::cloud::odbc_bq_driver_internal
