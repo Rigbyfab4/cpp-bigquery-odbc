f67c38de10cdc19561335d7ceb2a9855a1e13772
jsrinnn <114950032+jsrinnn@users.noreply.github.com>
2024-04-09 12:49:17 -0700

    impl(bq_driver): Added ANSI support for integration_tests testing utils (#311)

--- google/cloud/odbc/testing/odbc_utils/commons.h
+++ google/cloud/odbc/testing/odbc_utils/commons.h
@@ -153,11 +153,13 @@ class Table {
  public:
   Table(std::string table_name) { table_name_ = table_name; };
 
-  void Create(std::shared_ptr<ODBCHandles> conn, std::string schema_str);
+  void Create(std::shared_ptr<ODBCHandles> conn, std::string schema_str,
+              bool use_ansi = false);
 
-  void Drop(std::shared_ptr<ODBCHandles> conn);
+  void Drop(std::shared_ptr<ODBCHandles> conn, bool use_ansi = false);
 
-  void Insert(std::shared_ptr<ODBCHandles> conn, StdRows rows);
+  void Insert(std::shared_ptr<ODBCHandles> conn, StdRows rows,
+              bool use_ansi = false);
 
  private:
   std::string table_name_;
@@ -186,13 +188,16 @@ SQLRETURN PollODBC(Func odbc_api, ExponentialBackoffPolicy& backoff,
 // If there was an error, gets description from SQLGetDiagRec and throws an
 // error
 inline void CheckError(SQLRETURN status, std::string const api,
-                       std::shared_ptr<ODBCHandles> conn);
+                       std::shared_ptr<ODBCHandles> conn,
+                       bool use_ansi = false);
 
-void ExecuteStatement(std::shared_ptr<ODBCHandles> conn, char stmt[]);
+void ExecuteStatement(std::shared_ptr<ODBCHandles> conn, char stmt[],
+                      bool use_ansi = false);
 
 // Executes the SQLDescribeCol API to initialize the Column struct
 void DescribeCol(std::shared_ptr<ODBCHandles> conn,
-                 std::shared_ptr<Column> col_ptr, SQLUSMALLINT col_index);
+                 std::shared_ptr<Column> col_ptr, SQLUSMALLINT col_index,
+                 bool use_ansi = false);
 
 // Executes the BindCol API to bind the Column struct data buffers to the
 // statement handle
@@ -202,7 +207,8 @@ void BindCol(std::shared_ptr<ODBCHandles> conn, std::shared_ptr<Column> col_ptr,
 // The logic (internal implementation) of SQLBindCol
 // Works only with String type (also with null strings)
 void BindColManually(std::shared_ptr<ODBCHandles> conn,
-                     std::shared_ptr<Column> col_ptr, SQLUSMALLINT col_index);
+                     std::shared_ptr<Column> col_ptr, SQLUSMALLINT col_index,
+                     bool use_ansi = false);
 
 }  // namespace google::cloud::odbc_tests
 
