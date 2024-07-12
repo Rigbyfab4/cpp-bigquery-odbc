8fe4b994ed292cba6d915f91617773762d7d3043
Kanchan <kanchan.shukla@37848692.corp-partner.google.com>
2024-07-12 17:23:12 +0530

    updated test case

--- google/cloud/odbc/integration_tests/odbc_driver_tests/statement_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/statement_test.cc
@@ -1368,29 +1368,6 @@ TEST(SQLPrepareW, InsertQueryUnicode) {
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 }
 
-TEST(SQLPrepareW, UnicodeStatement_SQL_NTS) {
-  auto conn = std::make_shared<ODBCHandles>();
-
-  EXPECT_EQ(Connect(kDefaultConnectionString, conn), SQL_SUCCESS);
-
-  std::wstring query(
-      L"INSERT INTO INTEGRATION_TESTS.Test_Table VALUES(4, 'अच्छा', 28)\0");
-  std::vector<SQLWCHAR> sqlWStr(query.begin(), query.end());
- 
-
-  auto status = SQLPrepareW(conn->hstmt, sqlWStr.data(), SQL_NTS);
-  CheckError(status, "SQLPrepare", conn);
-
-#ifdef BQ_DRIVER_INTEGRATION_TESTS
-  // Cast hstmt to StatementHandle*
-  auto stmt_handle = static_cast<StatementHandle*>(conn->hstmt);
-
-  EXPECT_EQ(stmt_handle->GetStmtState(), StmtStates::kStatementPrepared);
-
-#endif
-  EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
-}
-
 TEST(SQLPrepareW, UnicodeStatementFailure) {
   auto conn = std::make_shared<ODBCHandles>();
 
