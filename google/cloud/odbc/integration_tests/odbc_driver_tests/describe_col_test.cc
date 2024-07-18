d0cdc12838d8d599b1f519bd0ac146561265a986
shivamd-gpartner <shivam.dabas@37848692.corp-partner.google.com>
2024-07-18 15:37:02 +0530

    Revert "fix checks"
    This reverts commit 5e1f1aecf6cd377631c062806ae0ee380e1b9a69.

--- google/cloud/odbc/integration_tests/odbc_driver_tests/describe_col_test.cc
+++ google/cloud/odbc/integration_tests/odbc_driver_tests/describe_col_test.cc
@@ -19,128 +19,14 @@
 
 namespace google::cloud::odbc_tests {
 
-struct ExpectedResults {
-  std::string bq_type;
-  SQLSMALLINT column_size_source = 0;
-  SQLSMALLINT decimal_digits_source = 0;
-};
-
-static std::vector<ExpectedResults> const kExpectedResults = {
-    {"STRING", SQL_DESC_LENGTH, SQL_DESC_SCALE},
-    {"BOOL", SQL_DESC_PRECISION, SQL_DESC_SCALE},
-    {"FLOAT64", SQL_DESC_LENGTH, SQL_DESC_SCALE},
-    {"INT64", SQL_DESC_LENGTH, SQL_DESC_SCALE},
-    {"TIMESTAMP", SQL_DESC_LENGTH, SQL_DESC_PRECISION},
-    {"BYTES", SQL_DESC_LENGTH, SQL_DESC_SCALE},
-    {"JSON", SQL_DESC_LENGTH, SQL_DESC_SCALE},
-    {"GEOGRAPHY", SQL_DESC_LENGTH, SQL_DESC_SCALE},
-    {"DATETIME", SQL_DESC_LENGTH, SQL_DESC_PRECISION},
 #ifndef BQ_DRIVER_INTEGRATION_TESTS
-    {"DATE", SQL_DESC_LENGTH, SQL_DESC_PRECISION},
-    {"INTERVAL", SQL_DESC_LENGTH, SQL_DESC_SCALE},
-    {"NUMERIC", SQL_DESC_LENGTH, SQL_DESC_SCALE},
-    {"BIGNUMERIC", SQL_DESC_PRECISION, SQL_DESC_SCALE},
-    {"RANGE<DATE>", SQL_DESC_LENGTH, SQL_DESC_SCALE},
-    {"RANGE<DATETIME>", SQL_DESC_LENGTH, SQL_DESC_SCALE},
-    {"RANGE<TIMESTAMP>", SQL_DESC_LENGTH, SQL_DESC_SCALE},
-    {"TIME", SQL_DESC_LENGTH, SQL_DESC_PRECISION},
-    {"STRUCT<x INT64, y STRING>", SQL_DESC_LENGTH, SQL_DESC_SCALE},
-    {"ARRAY<INT64>", SQL_DESC_LENGTH, SQL_DESC_SCALE},
-#endif
-};
-
-void ValidatePrecision(std::shared_ptr<ODBCHandles> conn,
-                       SQLSMALLINT column_number, SQLSMALLINT expected) {
-  SQLSMALLINT out_desc_precision;
-  SQLRETURN status =
-      SQLGetDescField(conn->ird, column_number, SQL_DESC_PRECISION,
-                      &out_desc_precision, 0, nullptr);
-  CheckError(status, "SQLGetDescField(SQL_DESC_PRECISION)", conn);
-  EXPECT_EQ(expected, out_desc_precision);
-}
-
-void ValidateScale(std::shared_ptr<ODBCHandles> conn, SQLSMALLINT column_number,
-                   SQLSMALLINT expected) {
-  SQLSMALLINT out_desc_scale;
-  SQLRETURN status = SQLGetDescField(conn->ird, column_number, SQL_DESC_SCALE,
-                                     &out_desc_scale, 0, nullptr);
-  CheckError(status, "SQLGetDescField(SQL_DESC_SCALE)", conn);
-  EXPECT_EQ(expected, out_desc_scale);
-}
-
-void ValidateLength(std::shared_ptr<ODBCHandles> conn,
-                    SQLSMALLINT column_number, SQLULEN expected) {
-  SQLSMALLINT out_desc_len;
-  SQLRETURN status = SQLGetDescField(conn->ird, column_number, SQL_DESC_LENGTH,
-                                     &out_desc_len, 0, nullptr);
-  CheckError(status, "SQLGetDescField(SQL_DESC_LENGTH)", conn);
-  EXPECT_EQ(expected, out_desc_len);
-}
-
-void ValidateExpectedResults(std::shared_ptr<ODBCHandles> conn,
-                             SQLCHAR column_name[kBufferLength],
-                             SQLSMALLINT column_name_Le,
-                             SQLSMALLINT column_number, SQLSMALLINT sql_type,
-                             SQLULEN column_size, SQLSMALLINT decimal_digits,
-                             SQLSMALLINT nullable) {
-  auto expected_result = kExpectedResults[column_number - 1];
-  SQLSMALLINT out_concise_c_type;
-  SQLRETURN status =
-      SQLGetDescField(conn->ird, column_number, SQL_DESC_CONCISE_TYPE,
-                      &out_concise_c_type, 0, nullptr);
-  CheckError(status, "SQLGetDescField(SQL_DESC_CONCISE_TYPE)", conn);
-  EXPECT_EQ(sql_type, out_concise_c_type);
-
-  if (expected_result.column_size_source == SQL_DESC_PRECISION) {
-    ValidatePrecision(conn, column_number, column_size);
-  } else if (expected_result.column_size_source == SQL_DESC_LENGTH) {
-    ValidateLength(conn, column_number, column_size);
-  }
-
-  if (expected_result.decimal_digits_source == SQL_DESC_PRECISION) {
-    ValidatePrecision(conn, column_number, decimal_digits);
-  } else if (expected_result.decimal_digits_source == SQL_DESC_SCALE) {
-    ValidateScale(conn, column_number, decimal_digits);
-  }
-
-  SQLSMALLINT out_nullable;
-  status = SQLGetDescField(conn->ird, column_number, SQL_DESC_NULLABLE,
-                           &out_nullable, 0, nullptr);
-  CheckError(status, "SQLGetDescField(SQL_DESC_NULLABLE)", conn);
-  EXPECT_EQ(nullable, out_nullable);
-
-  SQLCHAR out_column_Name[kBufferLength];
-  SQLINTEGER str_len = 0;
-  status = SQLGetDescField(conn->ird, column_number, SQL_DESC_NAME,
-                           &out_column_Name, kBufferLength, &str_len);
-  CheckError(status, "SQLGetDescField(SQL_DESC_NAME)", conn);
-  EXPECT_STREQ((char const*)out_column_Name, (char const*)column_name);
-  EXPECT_EQ(str_len, column_name_Le);
-}
-
-std::string CreateColumnName(int i) {
-  std::string name = "col_" + kExpectedResults[i].bq_type + " ";
-  std::replace_if(name.begin(), name.end(), ::ispunct, '_');
-  std::remove_if(name.begin(), name.end(), ::isblank);
-  return name;
-}
 
 TEST(SQLDescribeColumn, DescribeAllColumns) {
   auto conn = std::make_shared<ODBCHandles>();
   EXPECT_EQ(Connect(kDefaultConnectionString, conn, true), SQL_SUCCESS);
   auto table_name = kDatasetWithTablePrefix + "ODBC_DESCRIBE_COLUMNS_TEST";
   Table table(table_name);
-  std::string table_schema =
-      "(" + CreateColumnName(0) + kExpectedResults[0].bq_type;
-  std::string params = "?";
-  for (int i = 1; i < kExpectedResults.size(); i++) {
-    table_schema.append(", " + CreateColumnName(i) +
-                        kExpectedResults[i].bq_type);
-    params.append(", ?");
-  }
-  table_schema.append(")");
-  table.DropWithPrepare(conn);
-  table.CreateWithPrepare(conn, table_schema);
+  table.CreateWithPrepare(conn, getSchemaStr(kFullSchema));
 
   auto select_stmt = "SELECT * FROM " + table_name;
   auto status = SQLPrepare(conn->hstmt, (SQLCHAR*)select_stmt.c_str(), SQL_NTS);
@@ -197,5 +83,7 @@ TEST(SQLDescribeColumn, DescribeAllColumns) {
   table.Drop(conn);
   EXPECT_EQ(Disconnect(conn), SQL_SUCCESS);
 }
+
+#endif  // BQ_DRIVER_INTEGRATION_TESTS
 
 }  // namespace google::cloud::odbc_tests
