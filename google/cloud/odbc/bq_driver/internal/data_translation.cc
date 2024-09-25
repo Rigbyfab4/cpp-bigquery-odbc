1cc4b03e31f2849e7e0ec7bdb75f3764813db44a
gauvarsh <gaurav.varshney@37848692.corp-partner.google.com>
2024-09-25 17:42:36 +0530

    Change

--- google/cloud/odbc/bq_driver/internal/data_translation.cc
+++ google/cloud/odbc/bq_driver/internal/data_translation.cc
@@ -14,11 +14,11 @@
 
 #include "google/cloud/odbc/bq_driver/internal/data_translation.h"
 
+namespace google::cloud::odbc_bq_driver_internal {
+
 using google::cloud::odbc_internal::SQLStates;
 using google::cloud::odbc_internal::StatusRecord;
 using google::cloud::odbc_internal::StatusRecordOr;
-
-namespace google::cloud::odbc_bq_driver_internal {
 
 StatusRecord ConvertFromJsonDSValue(DSValue const& src_dsval,
                                     DataBuffer& dest_data) {
@@ -42,19 +42,9 @@ StatusRecord ConvertFromJsonDSValue(DSValue const& src_dsval,
             StatusRecord{SQLStates::k_HY000(), "Conversion Failed"};
         break;
       }
-      std::vector<SQLWCHAR> sql_w_str(wide_string->begin(), wide_string->end());
-      sql_w_str.emplace_back(L'\0');
-      auto* dest_val = static_cast<SQLWCHAR*>(dest_buf);
-
-      if (buffer_length > src_str.length()) {
-        *res_len = (sql_w_str.size() + 1) * sizeof(SQLWCHAR);
-
-        std::memcpy(dest_val, sql_w_str.data(),
-                    (sql_w_str.size() + 1) * sizeof(SQLWCHAR));
-      } else {
-        return StatusRecord{SQLStates::k_22003(),
-                            "Buffer length is insufficient"};
-      }
+      return WStrToOutputBufferResponse(
+          wide_string.GetValue(), dest_buf, buffer_length, src_str.length(),
+          src_str.length(), reinterpret_cast<SQLLEN*>(dest_data.result_len));
       break;
     }
     // TODO(b\367841053): SQL_C_BINARY to be done later
