f0810e2410aa84be4e4acc95344a35f8282520c1
NeerajDwivedii <165037147+NeerajDwivedii@users.noreply.github.com>
2026-03-30 19:29:10 +0530

    impl(bq_driver): add max_threads impl for UI (#1456)

--- google/cloud/odbc/bq_driver/internal/driver_form.cc
+++ google/cloud/odbc/bq_driver/internal/driver_form.cc
@@ -100,6 +100,7 @@ StatusRecord DriverForm::TestODBCConnection(
   std::optional<std::string> log_path;
   std::optional<int> log_file_count;
   std::optional<int> log_file_size;
+  std::optional<std::uint32_t> max_threads;
 
   auto safe_stoi = [](std::string const& value,
                       std::optional<int> fallback =
@@ -117,7 +118,11 @@ StatusRecord DriverForm::TestODBCConnection(
   log_file_count = safe_stoi(logging_section[kLogFileCount]);
   log_file_size = safe_stoi(logging_section[kLogFileSize]);
 
-  UpdateTraceOption(log_level, log_path, log_file_size, log_file_count);
+  max_threads = (*section)[kMaxThreadsParam].empty()
+                    ? kDefaultMaxThreads
+                    : std::stoull((*section)[kMaxThreadsParam]);
+  UpdateTraceOption(log_level, log_path, log_file_size, log_file_count,
+                    max_threads);
   // Build connection string
   std::string conn_string = BuildConnectionString(*section);
 
@@ -214,6 +219,7 @@ static Section BuildTestConnectionAttributes(
   attributes_map["LargeResultsTempTableExpirationTime"] =
       adv_form.GetTempTableExpiration();
   attributes_map["SessionLocation"] = adv_form.GetSessionLocation();
+  attributes_map["MaxThreads"] = adv_form.GetMaxThreads();
   attributes_map["AdditionalProjects"] = adv_form.GetAdditionalProjects();
   attributes_map["QueryProperties"] = adv_form.GetQueryProperties();
   attributes_map["HTAPI_ActivationThreshold"] =
@@ -481,23 +487,23 @@ void DriverForm::InitControls() {
                   kLabelWidth - 50, kLabelHeight, kIdcKeyFileHeader);
   SendMessage(h_key_file_path_header, WM_SETFONT, (WPARAM)h_font, TRUE);
   HWND h_key_file_edit =
-      CreateEditBox(m_hwnd, kAxisX + 170, kAxisY + 145, kEditComboBoxWidth,
+      CreateEditBox(m_hwnd, kAxisX + 170, kAxisY + 155, kEditComboBoxWidth,
                     kEditBoxHeight, kIdcKeyfileEdit);
   SendMessage(h_key_file_edit, WM_SETFONT, (WPARAM)h_font, TRUE);
   SetWindowSubclass(GetDlgItem(m_hwnd, kIdcKeyfileEdit), InputSubclassProc, 0,
                     0);
 
   HWND h_browse_button =
-      CreateButton(m_hwnd, "Browse...", kAxisX + 170, kAxisY + 170,
+      CreateButton(m_hwnd, "Browse...", kAxisX + 170, kAxisY + 190,
                    kBtnWidth + 8, kBtnHeight, kIdcBrowseButton);
   SendMessage(h_browse_button, WM_SETFONT, (WPARAM)h_font, TRUE);
-
-  HWND h_drive_scope_checkbox = CreateCheckBox(
-      m_hwnd, "Request Google Drive scope access", kAxisX + 5, kAxisY + 195,
-      kCheckboxWidth, kCheckboxHeight, kIdcDriveScopeCheckbox);
-  SendMessage(h_drive_scope_checkbox, WM_SETFONT, (WPARAM)h_font, TRUE);
-  SetWindowSubclass(GetDlgItem(m_hwnd, kIdcDriveScopeCheckbox),
-                    CheckboxSubclassProc, 0, 0);
+  // TODO(b/497725655): Enable UI feature after public release
+  // HWND h_drive_scope_checkbox = CreateCheckBox(
+  //     m_hwnd, "Request Google Drive scope access", kAxisX + 5, kAxisY + 195,
+  //     kCheckboxWidth, kCheckboxHeight, kIdcDriveScopeCheckbox);
+  // SendMessage(h_drive_scope_checkbox, WM_SETFONT, (WPARAM)h_font, TRUE);
+  // SetWindowSubclass(GetDlgItem(m_hwnd, kIdcDriveScopeCheckbox),
+  //                   CheckboxSubclassProc, 0, 0);
 
   HWND h_ssl_header =
       CreateGroupBox(m_hwnd, "SSL Options", kAxisX - 5, kAxisY + 250,
@@ -539,34 +545,36 @@ void DriverForm::InitControls() {
                    kBtnWidth + 8, kBtnHeight, kIdcTrustedCertBrowseButton);
   SendMessage(h_trusted_cert_browse_button, WM_SETFONT, (WPARAM)h_font, TRUE);
   HWND h_catalog_header =
-      CreateLabel(m_hwnd, "Project:", kAxisX, kAxisY + 385, kLabelWidth,
+      CreateLabel(m_hwnd, "Project:", kAxisX, kAxisY + 400, kLabelWidth,
                   kLabelHeight, WS_VISIBLE | SS_LEFT);
   SendMessage(h_catalog_header, WM_SETFONT, (WPARAM)h_font, TRUE);
   HWND h_catalog_box =
-      CreateComboBox(m_hwnd, kAxisX + 170, kAxisY + 385, kEditComboBoxWidth,
+      CreateComboBox(m_hwnd, kAxisX + 170, kAxisY + 400, kEditComboBoxWidth,
                      kComboBoxHeight, kIdcCatlogBOX);
   SendMessage(h_catalog_box, WM_SETFONT, (WPARAM)h_font, TRUE);
   SetWindowSubclass(GetDlgItem(m_hwnd, kIdcCatlogBOX), ComboBoxSubclassProc, 0,
                     0);
   HWND h_dataset_header =
-      CreateLabel(m_hwnd, "Dataset:", kAxisX, kAxisY + 413, kLabelWidth,
+      CreateLabel(m_hwnd, "Dataset:", kAxisX, kAxisY + 433, kLabelWidth,
                   kLabelHeight, WS_VISIBLE | SS_LEFT);
   SendMessage(h_dataset_header, WM_SETFONT, (WPARAM)h_font, TRUE);
   HWND h_dataset_box =
-      CreateComboBox(m_hwnd, kAxisX + 170, kAxisY + 413, kEditComboBoxWidth,
+      CreateComboBox(m_hwnd, kAxisX + 170, kAxisY + 433, kEditComboBoxWidth,
                      kComboBoxHeight, kIdcDatasetBOX);
   SendMessage(h_dataset_box, WM_SETFONT, (WPARAM)h_font, TRUE);
   SetWindowSubclass(GetDlgItem(m_hwnd, kIdcDatasetBOX), ComboBoxSubclassProc, 0,
                     0);
-  HWND h_gcp_parent_folder_header =
-      CreateLabel(m_hwnd, "GCP parent folder:", kAxisX, kAxisY + 441,
-                  kLabelWidth, kLabelHeight, WS_VISIBLE | SS_LEFT);
-  SendMessage(h_gcp_parent_folder_header, WM_SETFONT, (WPARAM)h_font, TRUE);
-  HWND h_gcp_parent_folder_text =
-      CreateEditBox(m_hwnd, kAxisX + 170, kAxisY + 441, kEditComboBoxWidth,
-                    kEditBoxHeight, kIdcGcpFolder);
-  SendMessage(h_gcp_parent_folder_text, WM_SETFONT, (WPARAM)h_font, TRUE);
-  SetWindowSubclass(GetDlgItem(m_hwnd, kIdcGcpFolder), InputSubclassProc, 0, 0);
+  // TODO(b/497725655): Enable UI feature after public release
+  // HWND h_gcp_parent_folder_header =
+  //     CreateLabel(m_hwnd, "GCP parent folder:", kAxisX, kAxisY + 441,
+  //                 kLabelWidth, kLabelHeight, WS_VISIBLE | SS_LEFT);
+  // SendMessage(h_gcp_parent_folder_header, WM_SETFONT, (WPARAM)h_font, TRUE);
+  // HWND h_gcp_parent_folder_text =
+  //     CreateEditBox(m_hwnd, kAxisX + 170, kAxisY + 441, kEditComboBoxWidth,
+  //                   kEditBoxHeight, kIdcGcpFolder);
+  // SendMessage(h_gcp_parent_folder_text, WM_SETFONT, (WPARAM)h_font, TRUE);
+  // SetWindowSubclass(GetDlgItem(m_hwnd, kIdcGcpFolder), InputSubclassProc, 0,
+  // 0);
 
   // This feature is turned off for the private release. It will be restored for
   // the public release with an accompanying documentation link.
--- google/cloud/odbc/bq_driver/odbc_windows.cc
+++ google/cloud/odbc/bq_driver/odbc_windows.cc
@@ -135,13 +135,14 @@ bool ConfigDSNInternal(HWND hwnd_parent, WORD f_request, LPCSTR lpsz_driver,
   std::string temp_expiration = GetValueOrDefault(section, temp_expiration_key);
   std::string session_location =
       GetValueOrDefault(section, session_location_key);
+  std::string max_threads = GetValueOrDefault(section, max_threads_key);
   std::string additional_projects =
       GetValueOrDefault(section, additional_projects_key);
   std::string query_properties =
       GetValueOrDefault(section, query_properties_key);
   std::string activation_threshold =
       GetValueOrDefault(section, activation_threshold_key);
-  std::string use_wchar = GetValueOrDefault(section, use_wchar_key);
+  // std::string use_wchar = GetValueOrDefault(section, use_wchar_key);
   std::string enable_session = GetValueOrDefault(section, enable_session_key);
   std::string htapi_activation_threshold_check =
       GetValueOrDefault(section, htapi_activation_threshold_check_key);
@@ -183,8 +184,9 @@ bool ConfigDSNInternal(HWND hwnd_parent, WORD f_request, LPCSTR lpsz_driver,
         {additional_projects_key, additional_projects},
         {query_properties_key, query_properties},
         {activation_threshold_key, activation_threshold},
-        {use_wchar_key, use_wchar},
+        // {use_wchar_key, use_wchar},
         {enable_session_key, enable_session},
+        {max_threads_key, max_threads},
         {htapi_activation_threshold_check_key,
          htapi_activation_threshold_check},
         {allow_large_results_key, allow_large_results},
@@ -240,8 +242,9 @@ bool ConfigDSNInternal(HWND hwnd_parent, WORD f_request, LPCSTR lpsz_driver,
     additional_projects = advance_form.GetAdditionalProjects();
     query_properties = advance_form.GetQueryProperties();
     activation_threshold = advance_form.GetActivationThreshold();
-    use_wchar = advance_form.GetUseWchar();
+    // use_wchar = advance_form.GetUseWchar();
     enable_session = advance_form.GetEnableSession();
+    max_threads = advance_form.GetMaxThreads();
     htapi_activation_threshold_check =
         advance_form.GetActivationThresholdCheckbox();
     allow_large_results = advance_form.GetAllowLargeResults();
